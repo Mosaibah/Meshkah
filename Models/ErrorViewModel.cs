8df331c1bb8031763e576ae672ad2afc1211d5da
Abdulrahman <abudrm2020@gmail.com>
2023-01-25 11:03:03 +0300

    create project, add gitignore file

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Meshkah.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
